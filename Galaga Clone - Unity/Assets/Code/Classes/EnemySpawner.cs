a189342ea57b93fc3fe92f68cc699e7fe81f9c8d
James Johnson <music1384@yahoo.co.uk>
2018-04-24 14:49:07 +0100

    Implement enemy spawner

---
+++ Galaga Clone - Unity/Assets/Code/Classes/EnemySpawner.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using Assets.Code.Enums;
+using Assets.Code.Classes.Enemies;
+using System.Linq;
+
+namespace Assets.Code.Classes
+{
+    class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private float _MinSpawnDelay = 0.5f;
+        [SerializeField] private float _MaxSpawnDelay = 1.25f;
+        [SerializeField] private Enemy[] _EnemyPrefabs = null;
+
+        private void Awake ()
+        {
+            OrderPrefabs ();
+        }
+
+        private void OrderPrefabs ()
+        {
+            // Order the enemy prefabs so that they're in a logical order.
+            _EnemyPrefabs = _EnemyPrefabs.OrderBy (x => (int)x.EnemyType).ToArray ();
+        }
+
+        private void Start ()
+        {
+            float spawnDelay = Random.Range (_MinSpawnDelay, _MaxSpawnDelay);
+            Invoke ("Spawn", spawnDelay);
+        }
+
+        private void Spawn ()
+        {
+            // Get the amount of different enemy types specified in the EnemyTypes enum.
+            int numberOfEnemyTypes = System.Enum.GetValues (typeof (EnemyTypes)).Length;
+            var enemyType = (EnemyTypes)Random.Range (0, numberOfEnemyTypes);
+
+            switch (enemyType)
+            {
+                case EnemyTypes.Meteor:
+                    SpawnMeteor ();
+                    break;
+                case EnemyTypes.Snowflake:
+                    SpawnSnowflakes ();
+                    break;
+                case EnemyTypes.Jelly:
+                    SpawnJellies ();
+                    break;
+            }
+
+            float spawnDelay = Random.Range (_MinSpawnDelay, _MaxSpawnDelay);
+            Invoke ("Spawn", spawnDelay);
+        }
+
+        private void SpawnMeteor ()
+        {
+            int index = (int)EnemyTypes.Meteor;
+            Instantiate (_EnemyPrefabs[index], new Vector3 (Random.Range (-9f, 9f), 15f, 0f), Quaternion.identity);
+        }
+
+        private void SpawnSnowflakes ()
+        {
+            int index = (int)EnemyTypes.Snowflake;
+            float xSpawnPos = Random.Range (-9f, 9f);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos, 15f, 0f), Quaternion.identity);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 1f, 16f, 0f), Quaternion.identity);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 2f, 17f, 0f), Quaternion.identity);
+        }
+
+        private void SpawnJellies ()
+        {
+            int index = (int)EnemyTypes.Jelly;
+            float xSpawnPos = Random.Range (-8f, 8f);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos, 15f, 0f), Quaternion.identity);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 1f, 14f, 0f), Quaternion.identity);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos - 1f, 14f, 0f), Quaternion.identity);
+        }
+    }
+}
