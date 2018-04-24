038b195a17f05bb297ef9dd8a47855631deda426
James Johnson <music1384@yahoo.co.uk>
2018-04-24 12:44:38 +0100

    Implemented enemy base class and meteor prefab

---
+++ Galaga Clone - Unity/Assets/Code/Classes/Enemies/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Classes.Enemies
+{
+    [RequireComponent(typeof (Rigidbody2D), typeof (Collider2D))]
+    abstract class Enemy : MonoBehaviour
+    {
+        [Tooltip ("The speed at which the enemy will fall.")]
+        [SerializeField] protected float _FallSpeed = 0f;
+        [Tooltip ("The Y-Axis boundary at which the enemy will be culled.")]
+        [SerializeField] protected float _CullBound = 0f;
+
+        protected Vector2 _Velocity = Vector2.zero;
+        protected Transform _Transform = null;
+        protected Rigidbody2D _Rigidbody2D = null;
+
+        protected virtual void Awake ()
+        {
+            AssignReferences ();
+            SetupRigidbody ();
+        }
+
+        protected virtual void AssignReferences ()
+        {
+            _Transform = GetComponent<Transform> ();
+            _Rigidbody2D = GetComponent<Rigidbody2D> ();
+            GetComponent<Collider2D> ().isTrigger = true;
+        }
+
+        protected void SetupRigidbody ()
+        {
+            _Rigidbody2D.isKinematic = false;
+            _Rigidbody2D.gravityScale = 0f;
+            _Rigidbody2D.freezeRotation = true;
+        }
+
+        protected virtual void FixedUpdate ()
+        {
+            Move ();
+
+            if (HasHitCullBound ())
+                Destroy (this.gameObject);
+        }
+
+        protected abstract void Move ();
+
+        protected bool HasHitCullBound ()
+        {
+            if (_Transform.position.y <= _CullBound)
+                return true;
+
+            return false;   
+        }
+    }
+}
---
+++ Galaga Clone - Unity/Assets/Code/Classes/Enemies/Meteor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Classes.Enemies
+{
+    class Meteor : Enemy
+    {
+        [Tooltip ("The minimum speed at which the meteor can fall.")]
+        [SerializeField] protected float _MinFallSpeed = 200f;
+        [Tooltip ("The maximum speed at which the meteor can fall.")]
+        [SerializeField] protected float _MaxFallSpeed = 300f;
+
+        protected override void AssignReferences ()
+        {
+            base.AssignReferences ();
+
+            _FallSpeed = Random.Range (_MinFallSpeed, _MaxFallSpeed);
+        }
+
+        protected override void Move ()
+        {
+            _Velocity = new Vector2 (_Rigidbody2D.velocity.x, -_FallSpeed) * Time.deltaTime;
+
+            _Rigidbody2D.velocity = _Velocity;
+        }
+    }
+}
