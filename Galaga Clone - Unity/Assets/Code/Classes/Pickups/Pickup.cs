8ab0b2c1a61e37716092ca58d2c10ea31457abb5
James Johnson <music1384@yahoo.co.uk>
2018-04-25 20:22:23 +0100

    Implement pickup base classes

---
+++ Galaga Clone - Unity/Assets/Code/Classes/Pickups/Pickup.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Classes.Pickups
+{
+    [RequireComponent (typeof (Rigidbody2D), typeof (Collider2D))]
+    abstract class Pickup : MonoBehaviour
+    {
+        [Tooltip ("The amount added to the score when this pickup is collected.")]
+        [SerializeField] protected uint _Score = 20;
+        [Tooltip ("How fast the object falls down the screen.")]
+        [SerializeField] protected float _FallSpeed = 300.0f;
+
+        /// <summary>Reference to this gameobjects Rigigbody2D component.</summary>
+        protected Rigidbody2D _Rigidbody2D = null;
+
+        protected virtual void Awake ()
+        {
+            AssignReferences ();
+            SetupRigidbody ();
+        }
+
+        protected void AssignReferences ()
+        {
+            _Rigidbody2D = GetComponent<Rigidbody2D> ();
+            GetComponent<Collider2D> ().isTrigger = true;
+        }
+
+        protected void SetupRigidbody ()
+        {
+            _Rigidbody2D.isKinematic = false;
+            _Rigidbody2D.gravityScale = 0f;
+            _Rigidbody2D.freezeRotation = false;
+        }
+
+        protected void Start ()
+        {
+            _Rigidbody2D.AddForce (Vector2.down * _FallSpeed, ForceMode2D.Impulse);
+        }
+
+        protected virtual void OnTriggerEnter2D (Collider2D other)
+        {
+            if (other.CompareTag ("Player"))
+            {
+                Collected ();
+                Destroy (this.gameObject);
+            }
+        }
+
+        protected abstract void Collected ();
+    }
+}
---
+++ Galaga Clone - Unity/Assets/Code/Classes/Pickups/TimedPickup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Classes.Pickups
+{
+    abstract class TimedPickup : Pickup
+    {
+        [Tooltip ("How long the pickup lasts for.")]
+        [SerializeField] protected float _Length = 1.0f;
+
+        protected override void OnTriggerEnter2D (Collider2D other)
+        {
+            if (other.CompareTag ("Player"))
+            {
+                Collected ();
+                Invoke ("PickupEnded", _Length);
+                Destroy (this.gameObject);
+            }
+        }
+
+        protected abstract void PickupEnded ();
+    }
+}
