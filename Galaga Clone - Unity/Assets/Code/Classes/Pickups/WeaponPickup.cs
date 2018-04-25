d21a8b6f27b3bfb3b9b7ec24cee193d6595c15ac
James Johnson <music1384@yahoo.co.uk>
2018-04-25 20:49:56 +0100

    Implement weapon pickups and prefabs

---
+++ Galaga Clone - Unity/Assets/Code/Classes/Pickups/WeaponPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Classes.Pickups
+{
+    class WeaponPickup : Pickup
+    {
+        [Tooltip ("The weapon type that this pickup will give to the player.")]
+        [SerializeField] protected Enums.WeaponType _WeaponType = Enums.WeaponType.Default;
+        [Tooltip ("The weapon prefab to spawn for the player.")]
+        [SerializeField] protected Weapons.TimedWeapon _Weapon = null;
+
+        protected override void Collected (Collider2D other)
+        {
+            var weapon = Instantiate (_Weapon, other.transform.position, Quaternion.identity);
+            weapon.transform.SetParent (other.transform);
+            other.GetComponent<PlayerController> ().SwitchWeapon (_WeaponType);
+        }
+    }
+}
