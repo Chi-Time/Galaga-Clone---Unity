0304b786712780de8ca6a4f0fb40e9ad55315fb1
James Johnson <music1384@yahoo.co.uk>
2018-04-25 21:33:39 +0100

    Implement one up pickup

---
+++ Galaga Clone - Unity/Assets/Code/Classes/Pickups/OneUpPickup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Classes.Pickups
+{
+    class OneUpPickup : Pickup
+    {
+        [Tooltip ("The number of lives given to the player upon collection.")]
+        [SerializeField] protected int _Lives = 1;
+
+        protected override void Collected (Collider2D other)
+        {
+            other.GetComponent<PlayerController> ().AddLives (_Lives);
+        }
+    }
+}
