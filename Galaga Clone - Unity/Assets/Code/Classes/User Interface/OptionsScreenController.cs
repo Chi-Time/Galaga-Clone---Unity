3d357b99115191ebf0a562144251feaf8c45386d
James Johnson <music1384@yahoo.co.uk>
2018-04-27 13:03:21 +0100

    Implement settings screen saving and loading as well as refactored

--- Galaga Clone - Unity/Assets/Code/Classes/User Interface/OptionsScreenController.cs
+++ Galaga Clone - Unity/Assets/Code/Classes/User Interface/OptionsScreenController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.Code.Classes.User_Interface
@@ -6,13 +7,107 @@ namespace Assets.Code.Classes.User_Interface
     class OptionsScreenController : MonoBehaviour
     {
         [SerializeField] private Text _VsyncLabel = null;
+        [SerializeField] private Toggle _VsyncToggle = null;
         [SerializeField] private Text _AntiAliasingLabel = null;
         [SerializeField] private Text _VisualQualityLabel = null;
+
+        private Settings _GameSettings = null;
+
+        private void OnEnable ()
+        {
+            GetSettings ();
+            SetAALabel ();
+            SetQualityLabel ();
+            SetVsyncLabel ();
+        }
+
+        private void GetSettings ()
+        {
+            if (File.Exists ("settings.json"))
+                _GameSettings = LoadSettingsFromFile ();
+            else
+                _GameSettings = LoadDefaultSettings ();
+        }
+
+        private Settings LoadSettingsFromFile ()
+        {
+            string settingsJSON = "";
+
+            using (var streamReader = new StreamReader ("settings.json", System.Text.Encoding.UTF8))
+            {
+                settingsJSON = streamReader.ReadToEnd ();
+            }
+
+            var settings = JsonUtility.FromJson<Settings> (settingsJSON);
+            return settings;
+        }
+
+        private Settings LoadDefaultSettings ()
+        {
+            var settings = new Settings
+            {
+                AALevel = QualitySettings.antiAliasing,
+                QualityLevel = QualitySettings.GetQualityLevel (),
+                VSyncCount = QualitySettings.vSyncCount
+            };
+
+            return settings;
+        }
+
+        private void SaveSettings ()
+        {
+            string settingsJSON = _GameSettings.SerializeToString ();
+
+            using (var streamWriter = new StreamWriter ("settings.json", false, System.Text.Encoding.UTF8))
+            {
+                streamWriter.Write (settingsJSON);
+            }
+        }
+
+        private void SetVsyncLabel ()
+        {
+            if (_GameSettings.VSyncCount == 0)
+            {
+                _VsyncToggle.isOn = false;
+                _VsyncLabel.text = "Vsync: Disabled";
+            }
+            else if (_GameSettings.VSyncCount == 1)
+            {
+                _VsyncToggle.isOn = true;
+                _VsyncLabel.text = "Vsync: Enabled";
+            }
+        }
+
+        private void SetAALabel ()
+        {
+            if (_GameSettings.AALevel == 0)
+                _AntiAliasingLabel.text = "Anti-Aliasing: Off";
+            else if (_GameSettings.AALevel == 2)
+                _AntiAliasingLabel.text = "Anti-Aliasing: 2X";
+            else if (_GameSettings.AALevel == 4)
+                _AntiAliasingLabel.text = "Anti-Aliasing: 4X";
+            else if (_GameSettings.AALevel == 8)
+                _AntiAliasingLabel.text = "Anti-Aliasing: 8X";
+        }
+
+        private void SetQualityLabel ()
+        {
+            if (_GameSettings.QualityLevel == 1)
+                _VisualQualityLabel.text = "Visual Quality: Low";
+            else if (_GameSettings.QualityLevel == 2)
+                _VisualQualityLabel.text = "Visual Quality: Medium";
+            else if (_GameSettings.QualityLevel == 3)
+                _VisualQualityLabel.text = "Visual Quality: High";
+            else if (_GameSettings.QualityLevel == 5)
+                _VisualQualityLabel.text = "Visual Quality: Ultra";
+        }
 
         public void ReturnToMenu (GameObject menuScreen)
         {
             menuScreen.SetActive (true);
             this.gameObject.SetActive (false);
+
+            SaveSettings ();
         }
 
         public void TogglyVSync (bool isEnabled)
@@ -20,41 +115,31 @@ namespace Assets.Code.Classes.User_Interface
             if (isEnabled)
             {
                 QualitySettings.vSyncCount = 1;
-                _VsyncLabel.text = "VSync: Enabled";
+                _GameSettings.VSyncCount = 1;
             }
             else
             {
                 QualitySettings.vSyncCount = 0;
-                _VsyncLabel.text = "VSync: Disabled";
+                _GameSettings.VSyncCount = 0;
             }
+
+            SetVsyncLabel ();
         }
 
         public void SetAntiAliasingLevel (int level)
         {
             QualitySettings.antiAliasing = level;
+            _GameSettings.AALevel = level;
 
-            if (level == 0)
-                _AntiAliasingLabel.text = "Anti-Aliasing: Off";
-            else if (level == 2)
-                _AntiAliasingLabel.text = "Anti-Aliasing: 2X";
-            else if (level == 4)
-                _AntiAliasingLabel.text = "Anti-Aliasing: 4X";
-            else if (level == 8)
-                _AntiAliasingLabel.text = "Anti-Aliasing: 8X";
+            SetAALabel ();
         }
 
         public void SetQualityLevel (int level)
         {
             QualitySettings.SetQualityLevel (level, true);
+            _GameSettings.QualityLevel = level;
 
-            if (level == 1)
-                _VisualQualityLabel.text = "Visual Quality: Low";
-            else if (level == 2)
-                _VisualQualityLabel.text = "Visual Quality: Medium";
-            else if (level == 3)
-                _VisualQualityLabel.text = "Visual Quality: High";
-            else if (level == 5)
-                _VisualQualityLabel.text = "Visual Quality: Ultra";
+            SetQualityLabel ();
         }
     }
 }
---
+++ Galaga Clone - Unity/Assets/Code/Classes/User Interface/Settings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Classes.User_Interface
+{
+    [System.Serializable]
+    class Settings
+    {
+        public int AALevel;
+        public int QualityLevel;
+        public int VSyncCount;
+
+        public string SerializeToString ()
+        {
+            return JsonUtility.ToJson (this, true);
+        }
+    }
+}
