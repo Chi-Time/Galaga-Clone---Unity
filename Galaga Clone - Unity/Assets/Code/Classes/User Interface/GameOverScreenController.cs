4605a8f4e163fd65bd4eba58ee8683aea25b64f2
James Johnson <music1384@yahoo.co.uk>
2018-04-27 13:54:14 +0100

    Implement game-over screen logic and prefab

---
+++ Galaga Clone - Unity/Assets/Code/Classes/User Interface/GameOverScreenController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Code.Classes.User_Interface
+{
+    sealed class GameOverScreenController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _SelectedButton = null;
+
+        private void OnEnable ()
+        {
+            EventSystem.current.SetSelectedGameObject (_SelectedButton.gameObject, null);
+        }
+
+        public void LoadScene (int level)
+        {
+            SceneManager.LoadScene (level);
+        }
+    }
+}
