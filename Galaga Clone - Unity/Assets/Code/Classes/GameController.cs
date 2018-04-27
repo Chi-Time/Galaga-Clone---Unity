09e247ad36ab02cbf946f3663d3b07574efb1ab6
James Johnson <music1384@yahoo.co.uk>
2018-04-27 16:53:18 +0100

    Implement score save/load system

--- Galaga Clone - Unity/Assets/Code/Classes/GameController.cs
+++ Galaga Clone - Unity/Assets/Code/Classes/GameController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Code.Classes
@@ -24,17 +25,68 @@ namespace Assets.Code.Classes
         [SerializeField] private uint _HighScore = 0;
         [SerializeField] private uint _Multiplier = 1;
 
+        private string _FileName = "savedata.json";
+
+        private void Awake ()
+        {
+            GetSave ();
+        }
+
+        private void GetSave ()
+        {
+            if (File.Exists (_FileName))
+                LoadSave ();
+        }
+
+        private void LoadSave ()
+        {
+            string saveDataJSON = "";
+
+            using (var streamReader = new StreamReader (_FileName, System.Text.Encoding.UTF8))
+            {
+                saveDataJSON = streamReader.ReadToEnd ();
+            }
+
+            var saveData = JsonUtility.FromJson<SaveData> (saveDataJSON);
+            _HighScore = saveData.HighScore;
+        }
+
         public void IncreaseScore (uint score)
         {
             _Score += score * Multiplier;
-
-            if (_Score >= _HighScore)
+            
+            if (IsNewHighScore ())
                 _HighScore = _Score;
+        }
+
+        private bool IsNewHighScore ()
+        {
+            if (_Score >= _HighScore)
+                return true;
+
+            return false;
         }
 
         public void GameOver ()
         {
+            if (IsNewHighScore ())
+                SaveNewHighScore ();
+
             SceneManager.LoadScene ((2));
+        }
+
+        private void SaveNewHighScore ()
+        {
+            var saveData = new SaveData
+            {
+                HighScore = _HighScore
+            };
+
+            string savaDataJSON = saveData.SerializeToString ();
+            using (var streamWriter = new StreamWriter (_FileName, false, System.Text.Encoding.UTF8))
+            {
+                streamWriter.Write (savaDataJSON);
+            }
         }
     }
 }
---
+++ Galaga Clone - Unity/Assets/Code/Classes/SaveData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Classes
+{
+    [System.Serializable]
+    class SaveData
+    {
+        public uint HighScore;
+
+        public string SerializeToString ()
+        {
+            return JsonUtility.ToJson (this, true);
+        }
+    }
+}
