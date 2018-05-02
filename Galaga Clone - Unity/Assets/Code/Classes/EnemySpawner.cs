2e401aaa5cff7a07cef1ff48602583c259107247
James Johnson <music1384@yahoo.co.uk>
2018-05-02 23:15:11 +0100

    Implement sprites and balance

--- Galaga Clone - Unity/Assets/Code/Classes/EnemySpawner.cs
+++ Galaga Clone - Unity/Assets/Code/Classes/EnemySpawner.cs
@@ -59,31 +59,31 @@ namespace Assets.Code.Classes
         private void SpawnMeteor ()
         {
             int index = (int)EnemyType.Meteor;
-            Instantiate (_EnemyPrefabs[index], new Vector3 (Random.Range (-9f, 9f), _SpawnHeight, 0f), Quaternion.identity);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (Random.Range (-9f, 9f), _SpawnHeight, 0f), _EnemyPrefabs[index].transform.rotation);
         }
 
         private void SpawnSnowflakes ()
         {
             int index = (int)EnemyType.Snowflake;
             float xSpawnPos = Random.Range (-8f, 8f);
-            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos, _SpawnHeight, 0f), Quaternion.identity);
-            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 1f, _SpawnHeight + 1f, 0f), Quaternion.identity);
-            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 2f, _SpawnHeight + 2f, 0f), Quaternion.identity);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos, _SpawnHeight, 0f), _EnemyPrefabs[index].transform.rotation);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 1.5f, _SpawnHeight + 2f, 0f), _EnemyPrefabs[index].transform.rotation);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 2.5f, _SpawnHeight + 4f, 0f), _EnemyPrefabs[index].transform.rotation);
         }
 
         private void SpawnJellies ()
         {
             int index = (int)EnemyType.Jelly;
             float xSpawnPos = Random.Range (-8f, 8f);
-            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos, _SpawnHeight, 0f), Quaternion.identity);
-            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 1f, _SpawnHeight - 1, 0f), Quaternion.identity);
-            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos - 1f, _SpawnHeight - 1, 0f), Quaternion.identity);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos, _SpawnHeight, 0f), _EnemyPrefabs[index].transform.rotation);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos + 1.5f, _SpawnHeight - 1.5f, 0f), _EnemyPrefabs[index].transform.rotation);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (xSpawnPos - 1.5f, _SpawnHeight - 1.5f, 0f), _EnemyPrefabs[index].transform.rotation);
         }
 
         private void SpawnTerran ()
         {
             int index = (int)EnemyType.Terran;
-            Instantiate (_EnemyPrefabs[index], new Vector3 (Random.Range (-8.5f, 8.5f), _SpawnHeight, 0f), Quaternion.identity);
+            Instantiate (_EnemyPrefabs[index], new Vector3 (Random.Range (-8.5f, 8.5f), _SpawnHeight, 0f), _EnemyPrefabs[index].transform.rotation);
         }
     }
 }
