cb7e6cd380b39670136493eb958b367b20f106d0
James Johnson <music1384@yahoo.co.uk>
2018-05-02 22:00:05 +0100

    Implement background music and scrolling background

---
+++ Galaga Clone - Unity/Assets/Code/Classes/ScrollTexture.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (Renderer))]
+public class ScrollTexture : MonoBehaviour
+{
+    [SerializeField] private Vector2 _Scroll = new Vector2 (0f, 1f);
+
+    private Vector2 _Offset = new Vector2 (0f, 0f);
+    private Renderer _Renderer = null;
+
+    private void Awake ()
+    {
+        AssignReferences ();
+    }
+
+    private void AssignReferences ()
+    {
+        _Renderer = GetComponent<Renderer> ();
+    }
+
+    private void FixedUpdate ()
+    {
+        _Offset += _Scroll * Time.deltaTime;
+        _Renderer.material.SetTextureOffset ("_MainTex", _Offset);
+    }
+}
