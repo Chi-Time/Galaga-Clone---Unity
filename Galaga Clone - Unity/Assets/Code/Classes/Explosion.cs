990f561973c208452d8702f409fc39eb1527bac3
James Johnson <music1384@yahoo.co.uk>
2018-05-02 23:44:44 +0100

    Implement animations, balance gameplay etc.

---
+++ Galaga Clone - Unity/Assets/Code/Classes/Explosion.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [Tooltip ("The sound clip played when the enemy dies.")]
+    [SerializeField] protected AudioClip _DeathSFX = null;
+
+    private void Awake ()
+    {
+        AudioSource.PlayClipAtPoint (_DeathSFX, Camera.main.transform.position, 1.0f);
+        Destroy (this.gameObject, 1.0f);
+    }
+}
